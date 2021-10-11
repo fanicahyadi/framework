193595be9bb38a5fbf410089b7b1366a6e605112
fanicahyadi <fanicahyadi0@gmail.com>
2021-10-11 12:15:38 +0700

    update halaman pertama

---
+++ app/Controllers/WebController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Controllers;
+
+class WebController extends BaseController
+{
+    public function index()
+    {
+        return view('beranda');
+    }
+}
---
+++ app/Views/beranda.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="description" content="The small framework with powerful features">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>BERANDA</title>
+</head>
+<body>
+Jumlah penduduk Kota Cirebon dalam 5 (lima) tahun terakhir terus mengalami peningkatan dari
+sisi kuantitas.
+Pada tahun 2017, penduduk kota Cirebon berjumlah 313.325 jiwa, bertambah 11.605 jiwa
+dibandingkan dengan tahun 2013 (301.720 jiwa).
+Jumlah penduduk laki-laki hampir sebanding dengan penduduk perempuan, yaitu 50,14 persen
+laki-laki dan 49,86 persen perempuan.
+Berdasarkan luas wilayah dibandingkan populasi penduduk, kepadatan penduduk Kota Cirebon
+rata-rata sekitar 8,387 orang per/km².
+Hasil proyeksi penduduk menurut kelompok umur 15-19 tahun yaitu 27.847 jiwa sementara
+paling sedikit ada pada kelompok umur 70-74 tahun yaitu 4817 jiwa.
+Kepadatan penduduk di Kota Cirebon dalam kurun waktu 5 (lima) tahun terakhir terus
+meningkat, dari 8,146 jiwa per km²
+pada tahun 2013 menjadi 8,387 jiwa per km² pada tahun 2017. Berdasarkan tingkat kepadatan
+penduduk, kepadatan tertinggi
+berada di Kecamatan Pekalipan sebesar 19,6 jiwa per km² dan yang terendah di Kecamatan
+Harjamukti sebesar 6,13 jiwa per km².
+Penduduk Kota Cirebon lebih didominasi oleh kelompok umur produktif (15-64 tahun), yaitu
+sebanyak 215.237 orang atau 69,32 persen
+dan kelompok umur yang tergolong tidak produktif sebanyak 95.249 orang atau 30,68 persen.
+Dengan adanya bonus demografi ini,
+Kota Cirebon diuntungkan dan memiliki peluang untuk dapat meningkatkan pertumbuhan
+produktifitas masyarakatnya.
+Bonus demografi diartikan secara sederhana adalah peluang (window of opportunity) yang
+dinikmati suatu wilayah sebagai
+akibat dari besarnya proporsi penduduk produktif. Walaupun data menunjukan 69,32 persen
+dari total jumlah penduduk Kota Cirebon adalah usia angkatan kerja (usia produktif), namun
+kualitasnya perlu terus ditingkatkan, sehingga mampu bersaing di pasar kerja di Kota Cirebon.
+Sumber: Perda No 5 Tahun 2019 tentang Rencana Pembangunan Jangka Menengah Daerah Kota
+Cirebon 2018-2023
+</body>
+</html>
